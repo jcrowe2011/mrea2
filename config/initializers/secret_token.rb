8bafb9e5b1aa00449afb1a335fa7c8fab2a84033
Joshua <jcrowe@interpract.com>
2012-07-01 02:13:47 -0700

    initial commit + README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrea::Application.config.secret_token = '246cd352afe51ae25f9a63c86e998e4c187fd72033c16b4c027a1ad304f880e93a8f15f2c1cac19fe1b8a9763cb75e8eed05e7b1f7d39c73d7b18fc939c85f53'
