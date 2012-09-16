0188045b309eaa38697e8bc4ac4056aef3c26ca2
Joshua Crowe <jcrowe@interpract.com>
2012-09-16 00:25:53 -0700

    changes made

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,32 +2,5 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'listings'" do
-    it "returns http success" do
-      get 'listings'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'profile'" do
-    it "returns http success" do
-      get 'profile'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'dashboard'" do
-    it "returns http success" do
-      get 'dashboard'
-      response.should be_success
-    end
-  end
 
 end
