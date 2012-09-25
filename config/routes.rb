3e191d7805fcbd5cab1ec8cd7df875a0214d629a
Joshua Crowe <jcrowe@interpract.com>
2012-09-24 22:41:52 -0700

    added a sessions controller and a sign in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Mrea::Application.routes.draw do
+  
 
   resources :users
+  resources :sessions,:only => [:new, :create, :destroy]
 
     root :to => 'users#new'
     
@@ -9,4 +11,6 @@ Mrea::Application.routes.draw do
     match '/listings',  :to => 'pages#listings'
     match '/help',      :to => 'pages#help'
     match '/signup',    :to => 'users#new'
+    match '/signin',    :to => 'sessions#new'
+    match 'signout',    :to => 'sessions#destroy'
 end
